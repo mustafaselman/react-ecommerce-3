fd2a287e683b6e10b4aa0c773d6add5007d0daff
mustafaselmanonal <mustafaselman.100@gmail.com>
2024-01-19 21:45:50 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Header from './components/header/Header'
+import Home from './pages/home/Home'
+import Contact from './pages/contact/Contact'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
+    </>
   )
 }
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,58 @@
 //// tüm linklerin olduğu başlık kısmı
 import React from 'react'
+import styles from "./Header.module.scss"
+import { Link } from 'react-router-dom'
+import { FaShoppingCart } from "react-icons/fa";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
 
 const Header = () => {
+
+  const logo = (
+    <div className={styles.logo}>
+      <Link to="/">
+        <h2>
+          e<span>Shop</span>
+        </h2>
+      </Link>
+    </div>
+  )
+
+  const cart = (
+    <span className={styles.cart}>
+      <Link to="/cart">
+        Cart
+        <FaShoppingCart size={20}/>
+        <p>0</p>
+      </Link>
+    </span>
+  )
   return (
-    <div>Header</div>
+    <header>
+      <div className={styles.header}>
+        {logo}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+          </ul>
+          <div className={styles["header-right"]}>
+            <span className={styles.links}>
+              <Link to="/login">Login</Link>
+              <Link to="/order-history">My Orders</Link>
+            </span>
+            {cart}
+          </div>
+        </nav>
+        <div className={styles["menu-icon"]}>
+          {cart}
+          <HiOutlineMenuAlt3 size={28}/>
+        </div>
+      </div>
+    </header>
   )
 }
 
