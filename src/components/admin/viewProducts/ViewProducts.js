a55eb8b91d210c81d69c016c8ccfe670f710a981
mustafaselmanonal <mustafaselman.100@gmail.com>
2024-02-12 22:46:07 +0300

    12.02.2024

--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -1,5 +1,5 @@
 //// admin panelindeki allproducts componenti
-import React from 'react'
+import React, { useEffect } from 'react'
 import {toast} from "react-toastify"
 import {collection, deleteDoc, doc, onSnapshot, orderBy, query} from "firebase/firestore"
 import { db, storage } from '../../../firebase/config'
@@ -9,48 +9,59 @@ import { Link } from 'react-router-dom'
 import {FaEdit,FaTrashAlt} from "react-icons/fa"
 import { deleteObject, ref } from 'firebase/storage'
 import Notiflix from 'notiflix'
-import {useDispatch} from "react-redux"
-import { STORE_PRODUCTS } from '../../../redux/slice/productSlice'
+import {useDispatch, useSelector} from "react-redux"
+import { STORE_PRODUCTS, selectProducts } from '../../../redux/slice/productSlice'
+import useFetchCollection from '../../../customHooks/useFetchCollection'
 
 const ViewProducts = () => {
 
-  const [products,setProducts] = React.useState([])
-  const [isLoading,setIsLoading] = React.useState([])
+  // const [products,setProducts] = React.useState([])
+  // const [isLoading,setIsLoading] = React.useState([])
+
+  const { data , isLoading } = useFetchCollection("products")
+
+  const products = useSelector(selectProducts)
 
   const dispatch = useDispatch();
 
-  React.useEffect(()=>{
-    getProducts()
-  },[])
+  // React.useEffect(()=>{
+  //   getProducts()
+  // },[])
 
-  const getProducts = () => {
-    setIsLoading(true)
-    try{
-      //belirli bir koleksiyonu almak için
-      const productsRef = collection(db, "products")
-      //seçtiğimiz koleksiyonu sırala
-      const q = query(productsRef, orderBy("createdAt","desc"))
-      //gerçek zamanlı dinleme
-      onSnapshot(q,(snapshot)=>{
-        // console.log(snapshot)
-        // console.log(snapshot.docs[0].data())
-        const allProducts = snapshot.docs.map((doc)=> {
-          return ({
-          id:doc.id,
-          ...doc.data()
-        })})
-        // console.log(allProducts)
-        setIsLoading(false)
-        setProducts(allProducts)
-        dispatch(STORE_PRODUCTS({products:allProducts}
-        ))
-      })
-    }
-    catch(error){
-      setIsLoading(false)
-      toast.error(error.message)
-    }
-  }
+  // const getProducts = () => {
+  //   setIsLoading(true)
+  //   try{
+  //     //belirli bir koleksiyonu almak için
+  //     const productsRef = collection(db, "products")
+  //     //seçtiğimiz koleksiyonu sırala
+  //     const q = query(productsRef, orderBy("createdAt","desc"))
+  //     //gerçek zamanlı dinleme
+  //     onSnapshot(q,(snapshot)=>{
+  //       // console.log(snapshot)
+  //       // console.log(snapshot.docs[0].data())
+  //       const allProducts = snapshot.docs.map((doc)=> {
+  //         return ({
+  //         id:doc.id,
+  //         ...doc.data()
+  //       })})
+  //       // console.log(allProducts)
+  //       setIsLoading(false)
+  //       setProducts(allProducts)
+  //       dispatch(STORE_PRODUCTS({products:allProducts}
+  //       ))
+  //     })
+  //   }
+  //   catch(error){
+  //     setIsLoading(false)
+  //     toast.error(error.message)
+  //   }
+  // }
+
+  useEffect(()=>{
+    dispatch(STORE_PRODUCTS({
+      products: data
+    }))
+  },[dispatch,data])
 
   const confirmDelete = (id,imageURL) => {
     Notiflix.Confirm.show(
--- src/redux/store.js
+++ src/redux/store.js
@@ -9,7 +9,12 @@ const rootReducer = combineReducers({
 })
 
 const store = configureStore({
-    reducer: rootReducer
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+
 })
 
 export default store
