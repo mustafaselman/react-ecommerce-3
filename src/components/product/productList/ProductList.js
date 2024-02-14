97abc1c8be2ec0a8677b629e4a7c45c0665d78ab
mustafaselmanonal <mustafaselman.100@gmail.com>
2024-02-14 22:41:49 +0300

    14.02.2024

--- src/components/product/productList/ProductList.js
+++ src/components/product/productList/ProductList.js
@@ -3,11 +3,14 @@ import React, { useState } from "react";
 import styles from "./ProductList.module.scss";
 import { BsFillGridFill } from "react-icons/bs";
 import { FaListAlt } from "react-icons/fa";
+import Search from "../../search/Search";
+import ProductItem from "../productItem/ProductItem";
 
-const ProductList = () => {
+const ProductList = ({products}) => {
   const [grid, setGrid] = useState(true);
+  const [search,setSearch] = useState("")
   return (
-    <div className={styles["product-list"]} id="product">
+    <div className={styles["product-list"]} id="products">
       <div className={styles.top}>
         <div className={styles.icons}>
           <BsFillGridFill
@@ -22,7 +25,7 @@ const ProductList = () => {
         </div>
         <div>
           <p>
-            <p>Search</p>
+            <Search value={search} onChange={(e)=>setSearch(e.target.value)}/>
           </p>
         </div>
         <div className={styles.sort}>
@@ -35,6 +38,21 @@ const ProductList = () => {
             <option value="z-a">Z - A</option>
           </select>
         </div>
+      </div>
+      <div className={grid ? `${styles.grid}` : `${styles.list}`}>
+        {products.length === 0 ? (
+          <p>No product found</p>
+        ) : (
+          <>
+          {products.map((product)=>{
+            return (
+              <div key={product.id}>
+                <ProductItem {...product} grid={grid} product={product}/>
+              </div>
+            )
+          })}
+          </>
+        )}
       </div>
     </div>
   );
